509e8c3be5da1b8ddf1f33d2ac52bee3e6af85a9
Oliver Schulz <oschulz@mpp.mpg.de>
2017-10-22 10:02:27 +0200

    Require Julia v0.6, update CI configs, prepare for Julia v0.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 # This file is a part of MGDO.jl, licensed under the MIT License (MIT).
 
-@Base.Test.testset "Package MGDO" begin
+import Compat.Test
+Test.@testset "Package MGDO" begin
     include.([
     ])
 end
